90a95821fe65d2f953505cf6c92d8a1bfcd129b2
sarvesh873 <sarveshvarade873@gmail.com>
2022-09-09 19:08:00 +0530

    test

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node{
+    public :
+            int data;
+            Node* next;
+};
+
+int main(){
+    Node* node1 = new Node();
+    cout<<node1->data;
+
+    return 0;
+}
